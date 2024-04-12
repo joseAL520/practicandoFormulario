ec7ca327baa7a9ba7fbf7bed2f49666375503e9f
jose angel <jose.albelt1817@gmail.com>
2024-04-12 11:04:55 -0500

    estilo del apartado de graficas

--- src/app/inventari/dasboarh/dasboarh.component.ts
+++ src/app/inventari/dasboarh/dasboarh.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CartComponent } from "./cart/cart.component";
+import { GraficasComponent } from "./graficas/graficas.component";
 
 @Component({
     selector: 'app-dasboarh',
     standalone: true,
     templateUrl: './dasboarh.component.html',
     styleUrl: './dasboarh.component.css',
-    imports: [CartComponent]
+    imports: [CartComponent, GraficasComponent]
 })
 export  default class DasboarhComponent {
 
---
+++ src/app/inventari/dasboarh/graficas/graficas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventario-dasboarh-graficas',
+  standalone: true,
+  imports: [],
+  templateUrl: './graficas.component.html',
+  styleUrl: './graficas.component.css'
+})
+export class GraficasComponent {
+
+}
