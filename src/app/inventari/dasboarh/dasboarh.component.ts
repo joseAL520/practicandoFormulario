dcb3c8b76bf44f3285d26201894691ef1d1ce969
jose angel <jose.albelt1817@gmail.com>
2024-04-12 10:01:12 -0500

    dasboar cart

--- src/app/inventari/dasboarh/dasboarh.component.ts
+++ src/app/inventari/dasboarh/dasboarh.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CartComponent } from "./cart/cart.component";
 
 @Component({
-  selector: 'app-dasboarh',
-  standalone: true,
-  imports: [],
-  templateUrl: './dasboarh.component.html',
-  styleUrl: './dasboarh.component.css'
+    selector: 'app-dasboarh',
+    standalone: true,
+    templateUrl: './dasboarh.component.html',
+    styleUrl: './dasboarh.component.css',
+    imports: [CartComponent]
 })
 export  default class DasboarhComponent {
 
