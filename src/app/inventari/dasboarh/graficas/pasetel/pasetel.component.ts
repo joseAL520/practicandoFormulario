d1c5c1a5f2be6f63484e5b236c3c892f35b62f7f
jose angel <jose.albelt1817@gmail.com>
2024-04-12 15:19:11 -0500

    mejoras de diseno

--- src/app/inventari/dasboarh/graficas/pasetel/pasetel.component.ts
+++ src/app/inventari/dasboarh/graficas/pasetel/pasetel.component.ts
@@ -20,31 +20,22 @@ export class PasetelComponent {
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
 
   // Pie
-  public pieChartOptions: ChartConfiguration['options'] = {
-    plugins: {
-      legend: {
-        display: true,
-        position: 'top',
-      },
-      // datalabels: {
-      //   formatter: (value, ctx) => {
-      //     if (ctx.chart.data.labels) {
-      //       return ctx.chart.data.labels[ctx.dataIndex];
-      //     }
-      //     return '';
-      //   },
-      // },
-    },
-  };
-  public pieChartData: ChartData<'pie', number[], string | string[]> = {
-    labels: [['Download', 'Sales'], ['In', 'Store', 'Sales'], 'Mail Sales'],
+  // Radar
+  public radarChartOptions: ChartConfiguration['options'] = {};
+  public radarChartLabels: string[] = [
+    'Eating',
+    'Drinking',
+    'Sleeping',
+  ];
+
+  public radarChartData: ChartData<'radar'> = {
+    labels: this.radarChartLabels,
     datasets: [
-      {
-        data: [300, 500, 100],
-      },
+      { data: [65, 59, 90, 81, 56, 55, 40], label: 'Series A' },
+      { data: [28, 48, 40, 19, 96, 27, 100], label: 'Series B' },
     ],
   };
-  public pieChartType: ChartType = 'pie';
+  public radarChartType: ChartType = 'radar';
 
   // events
   public chartClicked({
@@ -66,106 +57,5 @@ export class PasetelComponent {
   }): void {
     console.log(event, active);
   }
-
-  changeLabels(): void {
-    const words = [
-      'hen',
-      'variable',
-      'embryo',
-      'instal',
-      'pleasant',
-      'physical',
-      'bomber',
-      'army',
-      'add',
-      'film',
-      'conductor',
-      'comfortable',
-      'flourish',
-      'establish',
-      'circumstance',
-      'chimney',
-      'crack',
-      'hall',
-      'energy',
-      'treat',
-      'window',
-      'shareholder',
-      'division',
-      'disk',
-      'temptation',
-      'chord',
-      'left',
-      'hospital',
-      'beef',
-      'patrol',
-      'satisfied',
-      'academy',
-      'acceptance',
-      'ivory',
-      'aquarium',
-      'building',
-      'store',
-      'replace',
-      'language',
-      'redeem',
-      'honest',
-      'intention',
-      'silk',
-      'opera',
-      'sleep',
-      'innocent',
-      'ignore',
-      'suite',
-      'applaud',
-      'funny',
-    ];
-    const randomWord = () => words[Math.trunc(Math.random() * words.length)];
-    this.pieChartData.labels = new Array(3).map(() => randomWord());
-
-    this.chart?.update();
-  }
-
-  addSlice(): void {
-    if (this.pieChartData.labels) {
-      this.pieChartData.labels.push(['Line 1', 'Line 2', 'Line 3']);
-    }
-
-    this.pieChartData.datasets[0].data.push(400);
-
-    this.chart?.update();
-  }
-
-  removeSlice(): void {
-    if (this.pieChartData.labels) {
-      this.pieChartData.labels.pop();
-    }
-
-    this.pieChartData.datasets[0].data.pop();
-
-    this.chart?.update();
-  }
-
-  changeLegendPosition(): void {
-    if (this.pieChartOptions?.plugins?.legend) {
-      this.pieChartOptions.plugins.legend.position =
-        this.pieChartOptions.plugins.legend.position === 'left'
-          ? 'top'
-          : 'left';
-    }
-
-    this.chart?.render();
-  }
-
-  toggleLegend(): void {
-    if (this.pieChartOptions?.plugins?.legend) {
-      this.pieChartOptions.plugins.legend.display =
-        !this.pieChartOptions.plugins.legend.display;
-    }
-
-    this.chart?.render();
-  }
-
-
 
 }
