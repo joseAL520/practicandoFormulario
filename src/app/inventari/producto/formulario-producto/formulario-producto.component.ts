1507e2941809b3533e19f658aeb6a590ecf6944c
jose angel <jose.albelt1817@gmail.com>
2024-04-16 15:46:14 -0500

    formulario reactivo de productos

--- src/app/inventari/producto/formulario-producto/formulario-producto.component.ts
+++ src/app/inventari/producto/formulario-producto/formulario-producto.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {  FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'inventario-producto-formulario-producto',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './formulario-producto.component.html',
   styleUrl: './formulario-producto.component.css'
 })
 export class FormularioProductoComponent {
+  nombre: string = "miguel";
+  formularioProducto: FormGroup;
+
+
+
+  constructor (
+    private form: FormBuilder
+  ){
+    this.formularioProducto = this.form.group({
+        idProducto: ['',[Validators.required]],
+        nombreProducto: ['',[Validators.required]],
+        marcaProducto: ['',[Validators.required]],
+        categoria: ['',[Validators.required]],
+        proveedor: ['',[Validators.required]],
+        descripcion: [''],
+    })
+  }
+
+  validator(controlName: string , tipoError:string){
+    return this.formularioProducto.get(controlName)?.hasError(tipoError) && this.formularioProducto.get(controlName)?.touched
+  }
+
+
+  addProduct(){
+   console.log(this.formularioProducto)
+  }
 
 }
